5476af0b1ef319789de918eafed3e9ebb32d36dd
temnik <temnik15@bk.ru>
2020-09-15 21:03:42 +0300

    added debug tag

--- app/src/main/java/ru/temnik/findpict/AppData.kt
+++ app/src/main/java/ru/temnik/findpict/AppData.kt
@@ -3,4 +3,8 @@ package ru.temnik.findpict
 object AppData {
     const val baseUrl = "https://pixabay.com"
     const val apiKey = "18103189-f1ecfa687903d71fc75cc581f"
+
+
+    //System
+    const val debugTag = "FindPict"
 }
