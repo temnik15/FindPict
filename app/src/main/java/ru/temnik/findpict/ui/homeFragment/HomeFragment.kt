afc9e18fca4cf45c9e7794b3ed46f2c895876dcd
temnik <temnik15@bk.ru>
2020-09-15 21:07:28 +0300

    added tv_appName?.visibility=View.GONE

--- app/src/main/java/ru/temnik/findpict/ui/homeFragment/HomeFragment.kt
+++ app/src/main/java/ru/temnik/findpict/ui/homeFragment/HomeFragment.kt
@@ -1,33 +1,57 @@
-package ru.temnik.findpict.ui
+package ru.temnik.findpict.ui.homeFragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
-import android.widget.RelativeLayout
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.home_fragment.*
 import kotlinx.android.synthetic.main.home_fragment.view.*
 import ru.temnik.findpict.ProfileView
 import ru.temnik.findpict.R
 import ru.temnik.findpict.ui.contentFragment.ContentFragment
-import ru.temnik.findpict.ui.contentFragment.dagger.DaggerContentPresenterComponent
+import ru.temnik.findpict.ui.contentFragment.ContentPresenter
 
 class HomeFragment : Fragment(), ProfileView {
     companion object {
         const val tag = "HomeFragment"
+        var contentPresenter:ContentPresenter? = null
     }
-    private val presenter = HomePresenter()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
-        view.tv_appName.visibility=View.GONE
+        setContentFragment()
+        val search = view.search_view as SearchView
+        configSearch(search)
+        retainInstance=true
+        return view
+    }
+
+
+
+    private fun configSearch(search:SearchView){
+        // search.imeOptions = EditorInfo.IME_ACTION_SEARCH
+        search.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+               if(query!=null && query!=""){
+                   activity?.tv_appName?.visibility=View.GONE
+                   contentPresenter?.search(query)
+               }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        //
+    }
+    private fun setContentFragment(){
         childFragmentManager
             .beginTransaction()
             .addToBackStack(ContentFragment.tag)
@@ -37,39 +61,10 @@ class HomeFragment : Fragment(), ProfileView {
                 ContentFragment.tag
             )
             .commit()
-
-        //
-        val search = view.search_view as SearchView
-        // search.imeOptions = EditorInfo.IME_ACTION_SEARCH
-        search.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-        //
-        retainInstance=true
-        return view
     }
 
-    override fun setErrorFragment() {
-        childFragmentManager
-            .beginTransaction()
-            .addToBackStack(ErrorFragment.tag)
-            .add(R.id.home_fragment_frame,ErrorFragment(),ErrorFragment.tag)
-            .commit()
+    override fun visibilityError(visible: Boolean) {
+
     }
 
-    override fun onResume() {
-        super.onResume()
-        presenter.onAttach(this)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        presenter.onDetach()
-    }
 }
