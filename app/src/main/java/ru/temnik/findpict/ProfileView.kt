d8f906ea6060cb215e2e7a421ecda12c8dd9c9e6
temnik <temnik15@bk.ru>
2020-09-24 03:18:54 +0300

    added methods (visibilityAppName and callback for onBackPressed)

--- app/src/main/java/ru/temnik/findpict/ProfileView.kt
+++ app/src/main/java/ru/temnik/findpict/ProfileView.kt
@@ -1,8 +1,11 @@
 package ru.temnik.findpict
 
+import android.content.Context
 
 
 interface ProfileView {
-    fun visibilityError(visible:Boolean = false)
+    fun visibilityError(visible:Boolean = false){}
     fun visibilityLoading(visible:Boolean = false){}
+    fun visibilityAppName(visible:Boolean = false){}
+    fun onBackPressed():Boolean
 }
