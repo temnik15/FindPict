3d80b77b56203d0295fb632df548dee6a6567ab2
temnik <temnik15@bk.ru>
2020-09-24 03:25:11 +0300

    fix use flag .isLoading and added Log.d

--- app/src/main/java/ru/temnik/findpict/ui/homeFragment/HomePresenter.kt
+++ app/src/main/java/ru/temnik/findpict/ui/homeFragment/HomePresenter.kt
@@ -1,4 +1,4 @@
-package ru.temnik.findpict.ui.contentFragment
+package ru.temnik.findpict.ui.homeFragment
 
 import android.util.Log
 import retrofit2.Call
@@ -12,50 +12,60 @@ import ru.temnik.findpict.network.NetworkService
 import ru.temnik.findpict.network.api.imageAPI.PhotoType
 import javax.inject.Inject
 
-class ContentPresenter @Inject constructor(adapter: ContentAdapter) : ProfilePresenter {
+class HomePresenter @Inject constructor(homeAdapter: HomeAdapter) : ProfilePresenter {
+    private val logName = "[HomePresenter]"
     private var profileView: ProfileView? = null
-    var contentAdapter: ContentAdapter = adapter
+    var homeAdapter: HomeAdapter = homeAdapter
+        private set
 
+    var isLoading = false
     var currentQuery: String = ""
         private set
     private var contentPage = 1
 
 
     fun search(query: String) {
-        contentPage=1
-        contentAdapter.updateItems(emptyList())
+        contentPage = 1
+        homeAdapter.updateItems(emptyList())
         profileView?.visibilityLoading(true)
         currentQuery = query
         loadContent(query)
     }
 
     fun loadContent(query: String) {
+        isLoading = true
         if (profileView != null) {
             NetworkService.getImageApi()
-                .getPageImages(AppData.apiKey, query, contentPage, PhotoType.ALL.name)
+                .getPageImages(AppData.API_KEY, query, contentPage, PhotoType.ALL.name)
                 .enqueue(object : Callback<ApiRootDTO> {
                     override fun onFailure(call: Call<ApiRootDTO>, t: Throwable) {
                         profileView?.visibilityLoading(false)
                         profileView?.visibilityError(true)
-                        Log.d(AppData.debugTag,"[ContentPresenter] Retrofit --> Запрос: \"$query\". Ошибка загрузки контента!")
+                        Log.d(
+                            AppData.DEBUG_TAG,
+                            "$logName Retrofit --> Запрос: \"$query\". Ошибка загрузки контента!"
+                        )
                         return
-                        // TODO("Ошибка, например нет интернета")
                     }
 
                     override fun onResponse(
                         call: Call<ApiRootDTO>,
                         response: Response<ApiRootDTO>
                     ) {
-                        // пересмотреть
                         profileView?.visibilityLoading(false)
                         profileView?.visibilityError(false)
-                        //
+
                         val items = response.body()?.hits
                         if (items != null) {
-                            contentAdapter.updateItems(items,true)
+                            if(isLoading){
+                                homeAdapter.updateItems(items, true)
+                            }
                             contentPage++
-                            contentAdapter.isLoading = false
-                            Log.d(AppData.debugTag,"[ContentPresenter] Retrofit --> Запрос: \"$query\". Загрузка контента выполнена! items.size = ${items.size}")
+                            isLoading = false
+                            Log.d(
+                                AppData.DEBUG_TAG,
+                                "$logName Retrofit --> Запрос: \"$query\". Загрузка контента выполнена! items.size = ${items.size}"
+                            )
                         }
                     }
                 })
