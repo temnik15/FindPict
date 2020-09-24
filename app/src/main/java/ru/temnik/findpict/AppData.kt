167d00785e253ed8f0f7368f6dde963610ae5bab
temnik <temnik15@bk.ru>
2020-09-24 03:16:54 +0300

    added many app variables (for sharedPref, debugtag, requestCode and other)

--- app/src/main/java/ru/temnik/findpict/AppData.kt
+++ app/src/main/java/ru/temnik/findpict/AppData.kt
@@ -1,10 +1,17 @@
 package ru.temnik.findpict
 
 object AppData {
-    const val baseUrl = "https://pixabay.com"
-    const val apiKey = "18103189-f1ecfa687903d71fc75cc581f"
+    const val BASE_URL = "https://pixabay.com"
+    const val API_KEY = "18103189-f1ecfa687903d71fc75cc581f"
 
 
     //System
-    const val debugTag = "FindPict"
+    const val APP_LOG_NAME = "[FindPict_APP]"
+    const val DEBUG_TAG = "FindPict"
+    const val REQUEST_CODE = 2703
+    const val DEFAULT_SAVE_IMAGES_DIRECTORY = "FindPict/images"
+
+    const val APP_SHARED_PREF = "FIND_PICT_SHARED_PREF"
+    const val SHARED_PREF_LAST_CACHE_REMOVAL = "LAST_CACHE_REMOVAL"
+    const val CACHE_FLUSH_PERIOD: Long = 1800000L // 30 min
 }
