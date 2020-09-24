3149fac5e1b4b481c262cdb3f5c372ef8a2a4782
temnik <temnik15@bk.ru>
2020-09-24 03:21:48 +0300

    refactor. remove all methods and added override method onBackPressed and method for clear cache app (.checkingLastCacheDelete())

--- app/src/main/java/ru/temnik/findpict/ui/MainActivity.kt
+++ app/src/main/java/ru/temnik/findpict/ui/MainActivity.kt
@@ -1,100 +1,82 @@
 package ru.temnik.findpict.ui
 
+
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
-import kotlinx.android.synthetic.main.activity_main.*
+import com.bumptech.glide.Glide
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.home_fragment.*
+import ru.temnik.findpict.AppData
 import ru.temnik.findpict.ProfileView
 import ru.temnik.findpict.R
+import ru.temnik.findpict.ui.detailsImageFragment.DetailsImageFragment
 import ru.temnik.findpict.ui.homeFragment.HomeFragment
-import java.io.File
+import java.util.*
 
-class MainActivity : AppCompatActivity(), ProfileView {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        if(savedInstanceState==null){
+        if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .addToBackStack(HomeFragment.tag)
-                .add(
-                    R.id.activity_main_frame,
-                    HomeFragment(),
-                    HomeFragment.tag
-                )
+                .add(R.id.activity_main_frame, HomeFragment.newInstance(), HomeFragment.tag)
                 .commit()
         }
+        checkingLastCacheDelete()
     }
 
     override fun onBackPressed() {
-        val adapter = HomeFragment.contentPresenter?.contentAdapter
-        if(adapter!=null && !adapter.isLoading && adapter.itemCount>0){
-            adapter.updateItems(emptyList())
-            tv_appName?.visibility=View.VISIBLE
-            iv_error?.visibility=View.GONE
-            tv_loading?.visibility=View.GONE
-        }else{
-            finish()
-        }
-    }
-
-    private fun clearApplicationData() {
-        val cache: File = cacheDir
-        val appDir = File(cache.getParent())
-        if (appDir.exists()) {
-            val children: Array<String> = appDir.list()
-            for (s in children) {
-                if (s != "lib") {
-                    deleteDir(File(appDir, s))
-                    Log.i(
-                        "TAG",
-                        "**************** File /data/data/APP_PACKAGE/$s DELETED *******************"
-                    )
+        val detailsFragment = supportFragmentManager.findFragmentByTag(DetailsImageFragment.tag)
+        val homeFragment = supportFragmentManager.findFragmentByTag(HomeFragment.tag)
+        if (detailsFragment == null && homeFragment != null &&
+            homeFragment is ProfileView
+        ) {
+            if (!homeFragment.onBackPressed()) {
+                finish()
+            } else homeFragment.visibilityAppName(true)
+        } else {
+            if (detailsFragment is ProfileView) {
+                if (!detailsFragment.onBackPressed()) {
+                    super.onBackPressed()
                 }
+            } else {
+                super.onBackPressed()
             }
         }
     }
 
-    private fun deleteDir(dir: File?): Boolean {
-        if (dir != null && dir.isDirectory) {
-            val children: Array<String> = dir.list()
-            for (i in children.indices) {
-                val success = deleteDir(File(dir, children[i]))
-                if (!success) {
-                    return false
-                }
-            }
-        }
-        if (dir != null) {
-            return dir.delete()
-        }
-        return false
-    }
 
-    override fun visibilityError(visible: Boolean) {
-        if(visible){
-            tv_appName?.visibility=View.GONE
-            iv_error?.visibility=View.VISIBLE
-        }else{
-            iv_error?.visibility=View.GONE
-        }
-    }
 
-    override fun visibilityLoading(visible: Boolean) {
-        if(visible){
-            tv_appName?.visibility=View.GONE
-            iv_error?.visibility=View.GONE
-            //Todo показать загрузку
-            tv_loading?.visibility=View.VISIBLE
-        }else{
-            //Todo спрятать загрузку
-            tv_loading?.visibility=View.GONE
-        }
-    }
+   private fun checkingLastCacheDelete(){
+       fun checkingCleaningPeriod(lastDateMs:Long):Boolean{
+           val currentDate = Date().time
+           return currentDate-lastDateMs>AppData.CACHE_FLUSH_PERIOD
+       }
+       val sharedPref = getSharedPreferences(AppData.APP_SHARED_PREF, MODE_PRIVATE)
+       val lastDateMs = sharedPref.getLong(AppData.SHARED_PREF_LAST_CACHE_REMOVAL,0)
+       if(checkingCleaningPeriod(lastDateMs)){
+           val editor = sharedPref.edit()
+           editor.putLong(AppData.SHARED_PREF_LAST_CACHE_REMOVAL,Date().time)
+           editor.apply()
+           Observable.fromCallable {
+               Glide.get(applicationContext).clearDiskCache()
+           }.subscribeOn(Schedulers.io()).subscribe()
+           Log.d(AppData.DEBUG_TAG,"${AppData.APP_LOG_NAME} Очистка кэша успешно проведена!")
+       }
+
+
+   }
+
+
 }
