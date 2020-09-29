704f235ea3dece10ce56a882e043fe02215819cc
temnik <temnik15@bk.ru>
2020-09-30 00:27:25 +0300

    implemented profileView methods for item visibility

--- app/src/main/java/ru/temnik/findpict/ui/homeFragment/HomeFragment.kt
+++ app/src/main/java/ru/temnik/findpict/ui/homeFragment/HomeFragment.kt
@@ -1,6 +1,7 @@
 package ru.temnik.findpict.ui.homeFragment
 
 import android.os.Bundle
+import android.os.Handler
 import android.transition.TransitionInflater
 import android.view.LayoutInflater
 import android.view.View
@@ -10,19 +11,22 @@ import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import kotlinx.android.synthetic.main.home_fragment.*
 import kotlinx.android.synthetic.main.home_fragment.view.*
 import ru.temnik.findpict.ProfileView
 import ru.temnik.findpict.R
 import ru.temnik.findpict.ui.detailsImageFragment.DetailsImageFragment
+import ru.temnik.findpict.ui.dialogFragments.DialogNotificationFragment
 import ru.temnik.findpict.ui.homeFragment.dagger.DaggerHomePresenterComponent
 
 
 class HomeFragment : Fragment(), ProfileView,
-    HomeAdapter.OnImageClickListener {
+    HomeAdapter.OnImageClickListener, SwipeRefreshLayout.OnRefreshListener {
     companion object {
         val tag = HomeFragment::class.java.simpleName
         val presenter: HomePresenter = DaggerHomePresenterComponent.create().getHomePresenter()
+        var errorVisible = View.GONE
         fun newInstance(): HomeFragment {
             return HomeFragment()
         }
@@ -39,11 +43,18 @@ class HomeFragment : Fragment(), ProfileView,
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (presenter.homeAdapter.itemCount > 0) visibilityAppName(false)
+        view_error_frame.visibility = errorVisible
         val recycler = view.recycler
         recycler.adapter = presenter.homeAdapter
         recycler.layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
         configSearch(view.view_search)
         addPaginationListener(recycler)
+        view_error.setOnClickListener {
+            presenter.loadContent()
+        }
+        view_swipe_refresh.setOnRefreshListener(this)
+        view_swipe_refresh.setColorSchemeResources(
+            R.color.default_refresh_color)
     }
 
     override fun onResume() {
@@ -58,12 +69,46 @@ class HomeFragment : Fragment(), ProfileView,
         super.onPause()
     }
 
-
     override fun visibilityError(visible: Boolean) {
         if (visible) {
-            view_error.visibility = View.VISIBLE
+            if (view_error_frame.visibility == View.VISIBLE) {
+                showNotificationDialog()
+            } else {
+                if (view_first_error.visibility == View.GONE) {
+                    view_error_frame.visibility = View.VISIBLE
+                }
+
+            }
+
         } else {
-            view_error.visibility = View.GONE
+            view_error_frame.visibility = View.GONE
+        }
+    }
+
+    private fun showNotificationDialog(
+        title: String = getString(R.string.home_fragment_default_failed_load_images),
+        message: String = getString(R.string.default_check_internet)
+    ) {
+        val fragment = DialogNotificationFragment.newInstance(
+            title,
+            message
+        )
+        fragment.show(childFragmentManager, fragment.tag)
+    }
+
+    override fun visibilityTextError(visible: Boolean) {
+        if (view_swipe_refresh.visibility == View.VISIBLE) {
+            Handler().postDelayed({ view_swipe_refresh.isRefreshing = false }, 500)
+            if (visible) {
+                showNotificationDialog()
+            }
+        }
+        if (visible) {
+            view_first_error.visibility = View.VISIBLE
+            view_swipe_refresh.visibility = View.VISIBLE
+        } else {
+            view_first_error.visibility = View.GONE
+            view_swipe_refresh.visibility = View.GONE
         }
     }
 
@@ -83,12 +128,21 @@ class HomeFragment : Fragment(), ProfileView,
         }
     }
 
+    override fun visibilityNoContent(visible: Boolean) {
+        if (visible) {
+            view_no_content.visibility = View.VISIBLE
+        } else {
+            view_no_content.visibility = View.GONE
+        }
+    }
+
     override fun onBackPressed(): Boolean {
         val adapter = presenter.homeAdapter
         return if (adapter.itemCount > 0) {
             presenter.isLoading = false
             adapter.updateItems(listOf())
             view_search.setQuery("", false)
+            visibilityError(false)
             true
         } else {
             false
@@ -97,9 +151,8 @@ class HomeFragment : Fragment(), ProfileView,
     }
 
 
-
-
     override fun onImageClick(holder: HomeAdapter.ViewHolder) {
+        errorVisible = view_error_frame.visibility
         val transitionView = holder.imageView
         val transitionName =
             context?.resources?.getString(R.string.default_transition_name) ?: "image_view"
@@ -123,8 +176,9 @@ class HomeFragment : Fragment(), ProfileView,
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (query != null && query != "") {
                     search.clearFocus()
-                    view_appName.visibility = View.GONE
-                    view_loading.visibility = View.VISIBLE
+                    visibilityAppName(false)
+                    visibilityTextError(false)
+                    visibilityLoading(true)
                     presenter.search(query)
                 }
                 return false
@@ -147,12 +201,14 @@ class HomeFragment : Fragment(), ProfileView,
                 val totalItemCount = manager.itemCount
                 if (!presenter.isLoading && presenter.homeAdapter.itemCount > 0) {
                     if ((visibleItemCount + firstVisibleItems + 30) >= totalItemCount) {
-                        presenter.loadContent(presenter.currentQuery)
+                        presenter.loadContent()
                     }
                 }
             }
         })
     }
 
-
+    override fun onRefresh() {
+        presenter.loadContent()
+    }
 }
